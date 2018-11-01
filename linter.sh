ae7aea8a8565c16d2c895180ee04b29ffd4cd91f
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-01 11:15:55 -0400

    Fixed typos

--- linter.sh
+++ linter.sh
@@ -8,11 +8,11 @@ flake8 urlparser.py
 echo "Starting lint on urlbuilder.py"
 pylint urlbuilder.py --score=no
 pycodestyle urlbuilder.py
-flake8 urlparser.py
+flake8 urlbuilder.py
 
 echo "Starting lint on urltester.py"
 pylint urltester.py --score=no
 pycodestyle urltester.py
-flake8 urlparser.py
+flake8 urltester.py
 
 echo "Finished linting all files"
