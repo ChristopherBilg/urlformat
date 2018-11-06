e71191c2f4baa7bd0eea9e7b0f4f4d0bef9d46a2
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-06 10:37:41 -0500

    Added method is_allowed to robotparser.py

--- robotparser.py
+++ robotparser.py
@@ -13,14 +13,24 @@ class RobotParser:
         Initialization method.
         """
         if file_ is None:
-            raise TypeError
+            raise TypeError("No valid file has been given.")
         self.file_ = file_
 
     def parse(self):
         """
         Method for parsing through and gaining information from the file.
         """
-        with open(self.file_, "r") as f:
-            lines = f.readlines()
+        with open(self.file_, "r") as file_:
+            lines = file_.readlines()
 
         print(lines)
+
+        # continue here
+
+    def is_allowed(self, url):
+        """
+        Boolean method to determine if a given url is valid to be
+        crawling the given website containing the current robots.txt
+        """
+
+        # continue here
