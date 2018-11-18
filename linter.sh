e972480e8b3f40eaace7dce9e5a8008b76d55bc3
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-18 08:14:34 -0500

    Updated disable options

--- linter.sh
+++ linter.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 echo "Starting lint on urlparser.py"
-pylint urlparser.py --score=no
+pylint urlparser.py --score=no --disable=C0111,R0912
 pycodestyle urlparser.py
 flake8 urlparser.py
 
 echo "Starting lint on urlbuilder.py"
-pylint urlbuilder.py --score=no
+pylint urlbuilder.py --score=no --disable=C0111,R0912
 pycodestyle urlbuilder.py
 flake8 urlbuilder.py
 
 echo "Starting lint on urltester.py"
-pylint urltester.py --score=no --disable=C0111
+pylint urltester.py --score=no --disable=C0111,R0912
 pycodestyle urltester.py
 flake8 urltester.py
 
 echo "Starting lint on urlerrors.py"
-pylint urlerrors.py --score=no --disable=C0111
+pylint urlerrors.py --score=no --disable=C0111,R0912
 pycodestyle urlerrors.py
 flake8 urlerrors.py
 
 echo "Starting lint on robotparser.py"
-pylint robotparser.py --score=no --disable=C0111
+pylint robotparser.py --score=no --disable=C0111,R0912
 pycodestyle robotparser.py
 flake8 robotparser.py
 
