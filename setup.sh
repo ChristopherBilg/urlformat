62c0129cdf99b4eefa6a0c22502b64b8852fbfba
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-23 09:23:30 -0500

    Updated for repositories

--- setup.sh
+++ setup.sh
@@ -9,5 +9,14 @@ rm -rf dist/
 echo "Removing urlformat.egg-info/ directory"
 rm -rf urlformat.egg-info/
 
-"Running setup.py command to generate previous directories"
-python setup.py sdist bdist_wheel
+echo "Running setup.py command to generate previous directories"
+python3 setup.py sdist bdist_wheel
+
+echo "Uploading the dist/ directory (distribution) to the Test PyPI server"
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+echo "Installing the newly uploading package from the Test PyPI server"
+python3 -m pip install --index-url https://test.pypi.org/simple/ urlformat
+
+echo "Uploading the dist/ directory (distribution) to the official PyPI server"
+twine upload dist/*
