437e5ee6bb05e570c5c48df1a2b9cd3c8b6b5f17
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-28 12:18:48 -0500

    Updated linter to include Google's yapf linter program

--- urlformat/linter.sh
+++ urlformat/linter.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 
-disable_options="--disable=C0111,R0903,R0912"
+disable_options="C0111,R0903,R0912"
 
 echo "Starting lint on urlparser.py"
-pylint urlparser.py --score=no $disable_options
+pylint urlparser.py --score=no --disable=$disable_options
 pycodestyle urlparser.py
 flake8 urlparser.py
+yapf --style pep8 --parallel --diff urlparser.py
 
 echo "Starting lint on urlbuilder.py"
-pylint urlbuilder.py --score=no $disable_options
+pylint urlbuilder.py --score=no --disable=$disable_options
 pycodestyle urlbuilder.py
 flake8 urlbuilder.py
+yapf --style pep8 --parallel --diff urlbuilder.py
 
 echo "Starting lint on urltester.py"
-pylint urltester.py --score=no $disable_options
+pylint urltester.py --score=no --disable=$disable_options
 pycodestyle urltester.py
 flake8 urltester.py
+yapf --style pep8 --parallel --diff urltester.py
 
 echo "Starting lint on urlerrors.py"
-pylint urlerrors.py --score=no $disable_options
+pylint urlerrors.py --score=no --disable=$disable_options
 pycodestyle urlerrors.py
 flake8 urlerrors.py
+yapf --style pep8 --parallel --diff urlerrors.py
 
 echo "Starting lint on robotparser.py"
-pylint robotparser.py --score=no $disable_options
+pylint robotparser.py --score=no --disable=$disable_options
 pycodestyle robotparser.py
 flake8 robotparser.py
+yapf --style pep8 --parallel --diff robotparser.py
 
 echo "Starting lint on urlspeedtester.py"
-pylint urlspeedtester.py --score=no $disable_options
+pylint urlspeedtester.py --score=no --disable=$disable_options
 pycodestyle urlspeedtester.py
 flake8 urlspeedtester.py
+yapf --style pep8 --parallel --diff urlspeedtester.py
 
 echo "Starting lint on ../setup.py"
-pylint ../setup.py --score=no $disable_options
+pylint ../setup.py --score=no --disable=$disable_options
 pycodestyle ../setup.py
 flake8 ../setup.py
+yapf --style pep8 --parallel --diff ../setup.py
 
 echo "Finished linting all files"
