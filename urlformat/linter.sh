04c4407c0ea988ab6f5021f6cb08a5e0a3af2783
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-29 07:06:07 -0500

    Vastly, vastly improved readability and reliability of linter.sh

--- urlformat/linter.sh
+++ urlformat/linter.sh
@@ -1,47 +1,21 @@
 #!/bin/bash
 
 disable_options="C0111,R0903,R0912"
+py_files=("urlparser.py"
+          "urlbuilder.py"
+          "urltester.py"
+          "urlerrors.py"
+          "robotparser.py"
+          "urlspeedtester.py"
+          "../setup.py")
 
-echo "Starting lint on urlparser.py"
-pylint urlparser.py --score=no --disable=$disable_options
-pycodestyle urlparser.py
-flake8 urlparser.py
-yapf --style pep8 --parallel --diff urlparser.py
-
-echo "Starting lint on urlbuilder.py"
-pylint urlbuilder.py --score=no --disable=$disable_options
-pycodestyle urlbuilder.py
-flake8 urlbuilder.py
-yapf --style pep8 --parallel --diff urlbuilder.py
-
-echo "Starting lint on urltester.py"
-pylint urltester.py --score=no --disable=$disable_options
-pycodestyle urltester.py
-flake8 urltester.py
-yapf --style pep8 --parallel --diff urltester.py
-
-echo "Starting lint on urlerrors.py"
-pylint urlerrors.py --score=no --disable=$disable_options
-pycodestyle urlerrors.py
-flake8 urlerrors.py
-yapf --style pep8 --parallel --diff urlerrors.py
-
-echo "Starting lint on robotparser.py"
-pylint robotparser.py --score=no --disable=$disable_options
-pycodestyle robotparser.py
-flake8 robotparser.py
-yapf --style pep8 --parallel --diff robotparser.py
-
-echo "Starting lint on urlspeedtester.py"
-pylint urlspeedtester.py --score=no --disable=$disable_options
-pycodestyle urlspeedtester.py
-flake8 urlspeedtester.py
-yapf --style pep8 --parallel --diff urlspeedtester.py
-
-echo "Starting lint on ../setup.py"
-pylint ../setup.py --score=no --disable=$disable_options
-pycodestyle ../setup.py
-flake8 ../setup.py
-yapf --style pep8 --parallel --diff ../setup.py
+for i in "${py_files[@]}"
+do
+    echo "Starting lint on ${i}"
+    pylint ${i} --score=no --disable=$disable_options
+    pycodestyle ${i}
+    flake8 ${i}
+    yapf --style pep8 --diff ${i}
+done
 
 echo "Finished linting all files"
